2dda3ea7348434d9649b199f609967e505082398
isaacszf <isaacsouzafernandes1@hotmail.com>
2022-11-26 04:15:57 -0300

    minor adjustments

--- app/quote/helpers.go
+++ app/quote/helpers.go
@@ -6,8 +6,9 @@ import (
     "log"
     "net/http"
 	"strings"
+    "time"
 
-	"github.com/PuerkitoBio/goquery"
+    "github.com/PuerkitoBio/goquery"
 )
 
 func getPageByUrlAndParse(url string) (*goquery.Document, error) {
@@ -19,29 +20,32 @@ func getPageByUrlAndParse(url string) (*goquery.Document, error) {
 	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
 	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
 
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-    
-    if res.StatusCode == 403 {
-        log.Println("🔁 403 ST | Trying Again...")
-        return getPageByUrlAndParse(url)
+    for {
+        time.Sleep(time.Second * 3)
+
+        res, err := http.DefaultClient.Do(req)
+        if err != nil {
+            return nil, err
+        }
+
+        if res.StatusCode == 403 {
+            log.Println("🔁 403 ST | Trying Again...")
+            res, _ = http.DefaultClient.Do(req)
+
+            continue
+        } else if res.StatusCode != 200{
+            t := fmt.Sprintf(
+                "Status Code error: %d | Status: %s", res.StatusCode, res.Status)
+            return nil, errors.New(t)
+        } else {
+            document, err := goquery.NewDocumentFromReader(res.Body)
+            if err != nil {
+                return nil, err
+            }
+
+            return document, nil
+        }
     }
-    
-	if res.StatusCode != 200 {
-		t := fmt.Sprintf(
-			"Status Code error: %d | Status: %s", res.StatusCode, res.Status)
-		return nil, errors.New(t)
-	}
-
-	document, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return document, nil
 }
 
 func getFirstElementByClass(doc *goquery.Document, class string) string {
